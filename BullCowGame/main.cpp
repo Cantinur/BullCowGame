e8f57596255a3efb0a162810303ef09ec7fa9569
Henrik Sandberg <henrik.a.sandberg@icloud.com>
2019-02-05 17:51:48 +0100

    Initial Commit

---
+++ BullCowGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BullCowGame
+//
+//  Created by Henrik Anthony Odden Sandberg on 2/5/19.
+//  Copyright © 2019 Henrik Anthony Odden Sandberg. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
